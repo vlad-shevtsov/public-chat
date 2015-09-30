cb1f56dc7652201239a2a9aa9465f649310d0ea6
MirrorkaYT <letsplayobzor@gmail.com>
2015-09-30 22:22:19 +0300

    bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
+  get 'auth/failure', to: "home#index"
   get 'signout', to: 'sessions#destroy', as: 'signout'
     resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]

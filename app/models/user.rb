5632f69f1c964b966ed64cd4d54603d2ce66458e
MirrorkaYT <letsplayobzor@gmail.com>
2015-10-01 15:32:30 +0300

    add users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.exists = true
       user.save!
     end
   end

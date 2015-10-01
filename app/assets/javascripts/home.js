f57fb5457a911d660ffd4f587a11d719f99957b1
MirrorkaYT <letsplayobzor@gmail.com>
2015-10-01 10:33:59 +0300

    bug fixes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,10 @@
 $(function(){
   $('.input').keypress(function(e){
-    if(e.which == 13){
+    if(e.which is 13){
+      alert('lol');
       if($('textarea').val().length>0){
         $.post('/messages/', { message: $('textarea').val() });
+
         $('textarea').val('');
       }
       e.preventDefault();

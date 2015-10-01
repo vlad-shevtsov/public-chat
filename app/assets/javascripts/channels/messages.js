83d248084cf82950875abd2af6780eaa50e328cb
MirrorkaYT <letsplayobzor@gmail.com>
2015-10-01 11:16:47 +0300

    bug fixes

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,9 +1,9 @@
-// App.messages = App.cable.subscriptions.create('MessagesChannel', {
-//   received: function(data) {
-//     alert('fck');
-//     $message = $('<div/>').addClass('message').attr('message-id', data.id).appendTo('.message-board');
-//     $user_info = $('<div/>').addClass('user-info').text(data.username).appendTo($message);
-//     $message_text = $('<div/>').text(data.message).addClass('message-text').appendTo($message);
+App.messages = App.cable.subscriptions.create('MessagesChannel', {
+  received: function(data) {
+    alert('fck');
+    $message = $('<div/>').addClass('message').attr('message-id', data.id).appendTo('.message-board');
+    $user_info = $('<div/>').addClass('user-info').text(data.username).appendTo($message);
+    $message_text = $('<div/>').text(data.message).addClass('message-text').appendTo($message);
 
-//   }
-// });
+  }
+});

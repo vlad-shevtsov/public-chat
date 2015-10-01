1515757c0bf222a4e8a5369c29dacdd24295e68a
MirrorkaYT <letsplayobzor@gmail.com>
2015-10-01 16:37:03 +0300

    add users

--- app/assets/javascripts/channels/sessions.js
+++ app/assets/javascripts/channels/sessions.js
@@ -1,6 +1,5 @@
 App.messages = App.cable.subscriptions.create('SessionsChannel', {
   received: function(data) {
-    console.log(data);
     if(data.create){
       $('<div/>').addClass('online-users').attr('user-id', data.id).text(data.name).appendTo('.participants');
     }else

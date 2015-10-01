eeaf28fe78395a2bfc1b5cff864241ef7fde33f0
MirrorkaYT <letsplayobzor@gmail.com>
2015-10-02 00:04:48 +0300

    fixed adding users

--- app/assets/javascripts/channels/sessions.js
+++ app/assets/javascripts/channels/sessions.js
@@ -1,7 +1,8 @@
 App.messages = App.cable.subscriptions.create('SessionsChannel', {
   received: function(data) {
     if(data.create){
-      $('<div/>').addClass('online-users').attr('user-id', data.id).text(data.name).appendTo('.participants');
+      if($(".online-users[user-id='"+ data.id +"']").length <= 0)
+        $('<div/>').addClass('online-users').attr('user-id', data.id).text(data.name).appendTo('.participants');
     }else
       $(".online-users[user-id='" + data.id + "']").remove();
   }
